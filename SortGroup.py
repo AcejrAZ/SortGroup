e5222a9a3c484b350fce5f1ab9c0cd03cb06aff1
unknown <venturf2@DTUMR8770X0B866.nala.roche.com>
2015-03-30 16:26:27 -0700

    Made more general no hard coded lines (minimal)

--- SortGroup.py
+++ SortGroup.py
@@ -1,51 +1,39 @@
 import csv,os,sys
 from collections import Counter
+mypath="C:\\Users\\venturf2\\Downloads\\RegistrationSP15.csv"
+number_groups=30
 
-class my_main(object):
-    def __init__(self):
-        self.filepath="C:\\Users\\venturf2\\Google Drive\\Theta Tau\\WesternRegion\\RegionalConference\\Regionals - 14Fall_PiD\\RegisteredStudents.csv"
-        csvfile = open(self.filepath, 'rb')
+class my_main():
+    def __init__(self, mypath,number_groups):
+        self.mypath=mypath
+        csvfile = open(mypath, 'rb')
         myreader = csv.reader(csvfile, delimiter=',')
 
         self.mydict = {}
-        chapters = []
-        majors = []
-        grads = []
-        genders = []
+        mylists = {}
         for i,row in enumerate(myreader):
-            if not i :
-                continue
-            name,gender,chapter,major,grad = row
-            self.mydict[name] = [chapter,major,grad,gender]
-            chapters.append(chapter)
-            majors.append(major)
-            grads.append(grad)
-            genders.append(gender)
-        self.chapters_count = Counter(chapters)
-        self.majors_count = Counter(majors)
-        self.grads_count = Counter(grads)
-        self.genders_count = Counter(genders)
-        self.chapter_names=self.chapters_count.keys()
-        self.major_names=self.majors_count.keys()
-        self.grad_names=self.grads_count.keys()
-        self.gender_names=self.genders_count.keys()
+            for j,item in enumerate(row):
+                if j!=0:
+                    if i == 0:
+                        mylists[j-1]=[item]
+                        continue
+                    mylists[j-1].append(item)
+            self.mydict[row[0]]=row[1:] #Need to check this
+        self.list_count={}
+        self.list_names={}
+        for i,num in enumerate(mylists):
+            self.list_count[i]=Counter(mylists[num])
+            self.list_names[i]=self.list_count[i].keys()
         total = len(self.mydict)
-        number_groups = 40
         self.grouplist = {}
         for group_num in range(number_groups):
             self.grouplist["name"+str(group_num)]=my_group("name"+str(group_num))
-            attributes=self.chapter_names,self.major_names,self.grad_names,self.gender_names
-            self.grouplist["name"+str(group_num)].add_attributes(attributes)
+            self.grouplist["name"+str(group_num)].add_attributes(self.list_names)
 
     def item_type(self,item_name):
-        if item_name in self.chapter_names:
-            return "chapters"
-        if item_name in self.major_names:
-            return "majors"
-        if item_name in self.grad_names:
-            return "grads"
-        if item_name in self.gender_names:
-            return "genders"
+        for num in self.list_names:
+            if item_name in self.list_names[num]:
+                return num
 
     def get_min_groups(self,item=None):
         "chapter,major,grad = item"
@@ -67,10 +55,10 @@ class my_main(object):
             self.mydict[member].append(best_group)
 
     def output(self):
-        filepath=os.path.splitext(self.filepath)[0]+"_OUT.csv"
+        filepath=os.path.splitext(self.mypath)[0]+"_OUT.csv"
         csvfile = open(filepath, 'wb')
         mywriter = csv.writer(csvfile, delimiter=',')
-        mywriter.writerow(["name","chapter","major","grad","gender","group"])
+        mywriter.writerow(["name","chapter","major","grad","role","gender","group","group2"])
         for member in self.mydict:
             out=[member]
             out.extend(self.mydict[member])
@@ -92,59 +80,38 @@ class my_main(object):
 
     def show_variance(self):
         for group in self.grouplist:
-            print Counter(self.grouplist[group].majors_in_group)
-            print Counter(self.grouplist[group].chapters_in_group)
-            print Counter(self.grouplist[group].grads_in_group)
-            print Counter(self.grouplist[group].genders_in_group)
+            for num in self.list_names:
+                print Counter(self.grouplist[group].list_names[num])
 
-class my_group(my_main):
+class my_group():
     def __init__(self,group_name):
         self.group_name=group_name
-        self.chapters_in_group=[]
-        self.majors_in_group=[]
-        self.grads_in_group=[]
-        self.genders_in_group=[]
+        self.items_in_group={}
         self.members_in_group=[]
-        self.list_names={
-                        "chapters_in_group":self.chapters_in_group,
-                        "majors_in_group":self.majors_in_group,
-                        "grads_in_group":self.grads_in_group,
-                        "genders_in_group":self.genders_in_group
-                        }
+        
     def add_attributes(self,attributes):
-        chapter_names,major_names,grad_names,gender_names=attributes
-        self.chapter_names=chapter_names
-        self.major_names=major_names
-        self.grad_names=grad_names
-        self.gender_names=gender_names
+        self.list_names=attributes
+        for i,item in enumerate(attributes):
+            self.items_in_group[i]=[]
 
     def group_length(self):
         return len(self.members_in_group)
 
     def add_person(self,member_name,member_items):
-        chapter,major,grad,gender = member_items
-        self.chapters_in_group.append(chapter)
-        self.majors_in_group.append(major)
-        self.grads_in_group.append(grad)
-        self.genders_in_group.append(gender)
+        for i,item in enumerate(member_items):
+            self.items_in_group[i].append(item)
         self.members_in_group.append(member_name)
 
     def item_type(self,item_name):
-        if item_name in self.chapter_names:
-            return "chapters"
-        if item_name in self.major_names:
-            return "majors"
-        if item_name in self.grad_names:
-            return "grads"
-        if item_name in self.gender_names:
-            return "genders"
+        for num in self.list_names:
+            if item_name in self.list_names[num]:
+                return num
 
     def item_in_group(self,item_name):
         list_type=self.item_type(item_name)
-        list_name=list_type+"_in_group"
-        in_list=self.list_names[list_name]
-        if item_name in in_list:
-            counts = Counter(in_list)
+        item_list=self.items_in_group[list_type]
+        if item_name in item_list:
+            counts = Counter(item_list)
             return counts[item_name]
         else: return 0
 
